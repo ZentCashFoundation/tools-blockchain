93751313c94f5e92ebe97b55f29bf3a9187d9063
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2021-11-11 23:48:24 +0100

    Tool Update

--- checkpoints/checkpoints-nodejs/checkpoints-add/checkpoint_generator_daemon.js
+++ checkpoints/checkpoints-nodejs/checkpoints-add/checkpoint_generator_daemon.js
@@ -39,7 +39,7 @@ for (let i = 0; i < height; i += checkpointEveryBlocks){
 	console.log("Getting block info for height " + i);
 	const blockHash = await daemon.blockHeaderByHeight(i)
 	console.log(blockHash.hash);
-	checkpoints.push(new Checkpoint('{'+ i + ',' + ' '  + '\"' + blockHash.hash + '\"' + '};'));
+	checkpoints.push(new Checkpoint('{'+ i + ',' + ' '  + '\"' + blockHash.hash + '\"' + '},'));
 }
 
   console.log("Checkpoints:\n" + JSON.stringify(checkpoints, false , 7));
