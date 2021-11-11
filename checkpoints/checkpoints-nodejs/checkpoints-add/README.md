a2b11d17544d56a348f6765bf94435f6e6b17623
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2021-11-11 22:37:17 +0100

    Tool Update

---
+++ checkpoints/checkpoints-nodejs/checkpoints-add/README.md
@@ -0,0 +1,19 @@
+# Checkpoint Generator for Zent Cash
+This NodeJS utility generates the current checkpoints.csv file from a daemon and saves it to disk.
+
+### Requirements
+
+- [export-to-csv](https://github.com/alexcaza/export-to-csv)
+
+### Setup
+
+It's super easy to set up. Just type.
+```bash
+npm install
+npm start-daemon or npm start-csv
+```
+
+### Development
+
+Donate: [ZTC] Ze4q8WJuHNp5ex1tmAG6596fJZ6cFacpoWuBekezHJm4XVspetrwrJdTy4d6B8woQgVwA7GizLzAd9T3xAJUqGd12kR7NumBh
+
---
+++ checkpoints/checkpoints-nodejs/checkpoints-add/checkpoint_generator_csv.js
@@ -0,0 +1,52 @@
+const CSV = require("export-to-csv"),
+	  fs = require('fs'),
+	  TurtleCoind = require('turtlecoin-rpc').TurtleCoind;
+
+const daemon = new TurtleCoind({
+	host: 'seedpro2.zent.cash', // ip address or hostname of the Telluriumd host
+	port: 21698, // what port is the RPC server running on
+	timeout: 30000, // request timeout
+	ssl: false // whether we need to connect using SSL/TLS
+});
+
+const csv_options = {
+	fieldSeparator: '-',
+	quoteStrings: '"',
+	decimalSeparator: '.',
+	showLabels: false,
+	showTitle: false,
+	title: '',
+	useTextFile: false,
+	useBom: false,
+	useKeysAsHeaders: false
+};
+
+const csvExporter = new CSV.ExportToCsv(csv_options);
+
+class Checkpoint {
+	constructor(height) {
+			this.height = height;
+	}
+}
+
+const checkpointEveryBlocks = 100;
+const checkpoints = [];
+
+daemon.blockCount().then(async (height) => {
+console.log("Got blockheight " + height + "!");
+
+for (let i = 0; i < height; i += checkpointEveryBlocks){ 
+	console.log("Getting block info for height " + i);
+	const blockHash = await daemon.blockHeaderByHeight(i)
+	console.log(blockHash.hash);
+	checkpoints.push(new Checkpoint(i + "," + blockHash.hash));
+}
+
+  console.log("Checkpoints:\n" + JSON.stringify(checkpoints, false , 7));
+	let csvdata = await csvExporter.generateCsv(checkpoints, true);
+
+	csvdata = csvdata.split('"').join(''); 
+
+	await fs.writeFileSync('checkpoints.csv', csvdata);
+	await console.log("New checkpoints.csv file has been written!");
+});
